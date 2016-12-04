0c41d9da558f41070fb78afcd69e4dc4a00f296a
defrag <defrag@VIRTUAL>
2016-12-04 19:39:26 +0100

    hello

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+*/
 namespace TestApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello, World!");
+
         }
     }
 }
